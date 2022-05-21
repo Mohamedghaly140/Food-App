866034485a08fe9a1aa967d60377c562ee825b39
mohamedghaly140 <mohamedghaly140@gmail.com>
2022-05-21 03:02:34 +0200

    added bottom sheet

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StyleSheet, SafeAreaView, StatusBar, Platform } from "react-native";
 import Header from "./src/components/Header";
 import Search from "./src/components/Search";
+import BottomSheetApp from "./src/components/BottomSheetApp/BottomSheetApp";
 
 const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle={isAndroid ? "light-content" : "dark-content"} />
-      <Header />
-      <Search />
-    </SafeAreaView>
+    <BottomSheetApp />
+    // <SafeAreaView style={styles.container}>
+    //   <StatusBar barStyle={isAndroid ? "light-content" : "dark-content"} />
+    //   <Header />
+    //   <Search />
+    // </SafeAreaView>
   );
 }
 
